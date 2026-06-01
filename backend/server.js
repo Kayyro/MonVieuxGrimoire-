d444cdb271c1b7eee45b1d13ced86361851cb0b0
Kayyro <robin.estebe8@epitech.eu>
2026-06-01 17:14:37 +0200

    Connexion MongoDB + modèle Book

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 const app = express();
 
 app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use((req, res) => {
     res.json({ message: 'Votre requête a bien été reçue !' });
