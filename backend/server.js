ed3dac217b5df1d930e6b4edc47241a30ab90ec0
Kayyro <robin.estebe8@epitech.eu>
2026-06-02 15:00:54 +0200

    Branchement routes book dans server.js + fix typo multer

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const userRoutes = require('./routes/user');
+const bookRoutes = require('./routes/book');
+const path = require('path');
 
 dotenv.config();
 
@@ -13,8 +15,9 @@ mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-
+app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/api/auth', userRoutes);
+app.use('/api/books', bookRoutes);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
